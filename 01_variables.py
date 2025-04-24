53c9f23fb8a63aa3250ef1ab66a78cc0e0b11bf2
Kevin Coraizaca <kevin.coraizaca@ucuenca.edu.ec>
2025-04-23 19:21:32 -0500

    first steps

---
+++ 01_variables.py
@@ -0,0 +1,11 @@
+print(len("Hello, World!"))
+print(type("Hello, World!"))
+print(type(str(2025)))
+int('2025')
+print(type(int('2025')))
+print(type(float('2025')))
+input("Enter a number: ")
+print(type(input("Enter a number: ")))
+
+
+
---
+++ hello.py
@@ -0,0 +1,20 @@
+# commentary
+
+print("Hello, World!")
+
+"""
+This is a simple Python script that prints "Hello, World!" to the console.
+It serves as a basic introduction to Python programming and demonstrates the use of the print function.
+"""
+
+print(type("Hello, World!"))
+print(type(1))
+print(type(1.0))
+print(type(True))
+print(type(None))
+print(type([]))
+print(type({}))
+print(type(()))
+
+print(sum([1, 2, 3]))
+
